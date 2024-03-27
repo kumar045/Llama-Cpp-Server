80831d6370ed60dd10b8bdbfcd959ca6eb2ce813
Shivam Kumar <35759554+kumar045@users.noreply.github.com>
2024-03-27 15:47:49 +0530

    Update function_call.py

--- function_call.py
+++ function_call.py
@@ -1,16 +1,22 @@
+import streamlit as st
 from openai import OpenAI
 import json
 import instructor
 
-# Create a client
-client = OpenAI(api_key="any random number or string", base_url="http://localhost:8000/v1")
-# Create a patched client
-client = instructor.patch(client=client)
-
-student_custom_functions = [
+def app():
+    # Set up the Streamlit app
+    st.title("NER")
+    
+    
+    client = OpenAI(api_key="asdf", base_url="http://localhost:8000/v1")
+        
+    # Create a patched client
+    client = instructor.patch(client=client)
+    
+    ticket_custom_functions = [
     {
-        'name': 'extract_student_info',
-        'description': 'Get the student information from the body of the input text',
+        'name': 'extract_passenger_info',
+        'description': 'Get the passenger information from the body of the input text',
         'parameters': {
             'type': 'object',
             'properties': {
@@ -18,38 +24,42 @@ student_custom_functions = [
                     'type': 'string',
                     'description': 'Name of the person'
                 },
-                'major': {
+                'orderID': {
                     'type': 'string',
-                    'description': 'Major subject.'
+                    'description': 'The order or booking ID.'
                 },
-                'school': {
+                'bookedOn': {
                     'type': 'string',
-                    'description': 'The university name.'
+                    'description': 'The date and time when the booking was made.'
                 },
-                'grades': {
+                'flightSupport': {
                     'type': 'integer',
-                    'description': 'GPA of the student.'
-                },
-                'club': {
-                    'type': 'string',
-                    'description': 'School club for extracurricular activities. '
+                    'description': 'The customer support number for flight-related queries.'
                 }
-                
             }
         }
     }
 ]
-student_1_description = "David Nguyen is a sophomore majoring in computer science at Stanford University. He is Asian American and has a 3.8 GPA. David is known for his programming skills and is an active member of the university's Robotics Club. He hopes to pursue a career in artificial intelligence after graduating."
-student_description = [student_1_description]
-for i in student_description:
-    response = client.chat.completions.create(
-        model = 'mistral-function-calling',
-        messages = [{'role': 'user', 'content': i}],
-        functions = student_custom_functions,
-        function_call = 'auto'
-    )
-
     
-# Loading the response as a JSON object
-json_response = json.loads(response.choices[0].message.function_call.arguments)
-print(json_response)
+    # Get the student description from the user
+    ticket_description = st.text_area("Enter the ticket description")
+    
+    if ticket_description:
+        if st.button('Extract'):
+
+            response = client.chat.completions.create(
+                model='stable-3b-function-calling',
+                messages=[{'role': 'user', 'content': ticket_description}],
+                functions= ticket_custom_functions,
+                function_call='auto'
+            )
+            
+            # Loading the response as a JSON object
+            json_response = json.loads(response.choices[0].message.function_call.arguments)
+            
+            # Display the extracted student information
+            st.write("Extracted Ticket Information:")
+            st.json(json_response)
+
+if __name__ == "__main__":
+    app()
